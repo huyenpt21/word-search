9930622c2eaf103ddc613bc8150b18e5af2d0640
HuyenPT <huyenpthe140045@fpt.edu.vn>
2023-02-21 11:31:47 +0700

    change hash router

--- src/router.js
+++ src/router.js
@@ -1,25 +1,25 @@
-import { DONG_AM, DONG_NGHIA, TRAI_NGHIA } from "constant/common"
-import MainLayout from "layout"
-import ContentComponent from "pages/contentPage"
-import { createBrowserRouter } from "react-router-dom"
+import { DONG_AM, DONG_NGHIA, TRAI_NGHIA } from "constant/common";
+import MainLayout from "layout";
+import ContentComponent from "pages/contentPage";
+import { createBrowserRouter, createHashRouter } from "react-router-dom";
 
-export const router = createBrowserRouter([
-    {
-        path: "",
-        element: <MainLayout/>,
-        children: [
-            {
-                path:DONG_AM,
-                element: <ContentComponent type="sl" />
-            },
-            {
-                path:DONG_NGHIA,
-                element: <ContentComponent type="ml" />
-            },
-            {
-                path:TRAI_NGHIA,
-                element: <ContentComponent type="sl" />
-            },
-        ]
-    }
-])
+export const router = createHashRouter([
+  {
+    path: "",
+    element: <MainLayout />,
+    children: [
+      {
+        path: DONG_AM,
+        element: <ContentComponent type="sl" />,
+      },
+      {
+        path: DONG_NGHIA,
+        element: <ContentComponent type="ml" />,
+      },
+      {
+        path: TRAI_NGHIA,
+        element: <ContentComponent type="sl" />,
+      },
+    ],
+  },
+]);
